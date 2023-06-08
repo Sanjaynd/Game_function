561b1a876a7bac2b9ec5b137c79d2167cfad08db
Sanjaynd <134504165+Sanjaynd@users.noreply.github.com>
2023-06-08 20:46:26 +0530

    Add files via upload

---
+++ game_function.py
@@ -0,0 +1,11 @@
+def sum(numbers): 
+	total = 0
+	for x in numbers:
+		total += x
+	return total
+print(sum((8, 2, 3, 0, 7))) 
+print("")
+print("")
+print("")
+
+print("Summation should like 8+2+3+0+7 = 20")
